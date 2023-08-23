fb1de864f9fd89b4fa3b60828d0e03fd1bedd8f8
patriciocarbonell <carbonellpato@gmail.com>
2023-08-23 19:15:31 -0300

    css login and register

--- js/login.js
+++ js/login.js
@@ -9,10 +9,7 @@ formulario.addEventListener("submit", function(event){
 /*Función que frena las acciones por defecto de un submit (recargar la página) */
     event.preventDefault();
 
-/*Funcion que almacenacena la información de que el usuario está logeado al enviar el formulario*/
-    sessionStorage.setItem("estaLogeado", true);
-
-/*Redirección a la página de inicio luego del envío del formulario*/
+/*Redurección a la página de inicio luego del envío del formulario*/
     window.location.href="index.html"
 });
 
